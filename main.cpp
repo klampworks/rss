ab71b73a6d46905b35955501554e9a590cb82a65
yukipyon <wolfvbear@gmail.com>
2013-11-24 15:10:41 +0000

    Removed hardcoded testing url.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@
 #include <iostream>
 int main(int argc, char **argv) {
 
+	if (argc < 2) {
+		
+		std::cout << "Pass the url of the RSS feed as the first argument." << std::endl;
+		exit(1);
+	}
+
+	//This is ok until we have a settings window.
+	const char *url = argv[1];
+
 	QApplication app(argc, argv);
 	rss_gui window;
 
-	std::string xml = rss_grabber::grab_xml("http://www.sankakucomplex.com/feed/");
+	std::string xml = rss_grabber::grab_xml(url);
 	std::cout << xml << std::endl;
 	std::vector<rss_item> items = rss_parser::parse_xml(xml);
 
