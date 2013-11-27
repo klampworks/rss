6f051376bf5602d77038d77db99ce2a3fa3964fe
yukipyon <wolfvbear@gmail.com>
2013-11-27 19:41:16 +0000

    Added suuport for downloading the first image of an article andcacheing the result.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,62 @@
 #include <QApplication>
 #include "rss_parser.hpp"
 #include "rss_grabber.hpp"
-
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+
+void proccess_img(std::string link) {
+
+	//TODO: This is nearly the same code repeated, refactor please.
+
+	const char *path = "cache/";
+	mkdir(path, 0777);
+
+	std::cout << link << std::endl;
+
+	std::string t_link = link.substr(0, link.length() - 1);
+	std::string name = path + t_link.substr(t_link.find_last_of('/')+1);
+
+	std::cout << name << std::endl;
+	std::ifstream ifs(name);
+	std::string xml;
+
+	if (!ifs.good()) {
+
+		//This page has not ben cached.
+		xml = rss_grabber::grab_xml(link.c_str());
+		ifs.close();
+
+		//Cache the text for next time.
+		std::ofstream ofs(name);
+		ofs << xml;
+		ofs.close();
+	} else {
+
+		//This page has been chached, read it into a string.
+		std::getline(ifs, xml, char(-1));
+		ifs.close();
+	}
+	
+	//Parse the link.
+	link = rss_parser::parse_img(xml);
+
+	t_link = link.substr(0, link.length() - 1);
+	name = path + link.substr(link.find_last_of('/')+1);
+
+	ifs.open(name);
+
+	if (!ifs.good()) {
+
+		//grab the image and save it as this name.
+		xml = rss_grabber::grab_xml(link.c_str());
+		std::ofstream ofs(name);
+		ofs << xml;
+		ofs.close();
+
+	} //else we already have this image, nevermind.
+}
+
 int main(int argc, char **argv) {
 
 	QApplication app(argc, argv);
@@ -31,10 +85,8 @@ int main(int argc, char **argv) {
 		window.add_item(item.title);
 		std::string link;
 		link.assign(item.link.begin(), item.link.end());
-		std::string xml = rss_grabber::grab_xml(link.c_str());
-		std::string url = rss_parser::parse_img(xml);
-		std::cout << url << std::endl;
-
+		
+		proccess_img(link);
 	}
 
 	window.show();
